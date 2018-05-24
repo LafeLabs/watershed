244c87fae85a59bd07a2009d8fffa67321c5df0d
Lafe Spietz <lafelabs@gmail.com>
2018-05-24 01:09:37 -0400

    better! direct link to the page from saver/publisher

--- replicators/page/savepage.php
+++ replicators/page/savepage.php
@@ -11,11 +11,14 @@
     $file = fopen("pages/index.html","w");// create new file with this name
     fwrite($file,"<p><a href = \"../?url=pages/".$filename."\">direct link to page saved at".gmdate("Y-m-d H:i:s", $timestamp)."(GMT) </a>\n"."(<a href = \"".$filename."\">".$filename."</a>)</p>".$oldfeed); //write data to file
     fclose($file);  //close file
+    
+    echo "<p><a href = \"../?url=pages/".$filename."\">direct link to page saved at".gmdate("Y-m-d H:i:s", $timestamp)."(GMT)</a></p>\n";
+    
 ?>
 <p>
-<a href = "pageeditor.php">pageeditor.php</a>
-<br/>
 <a href = "pages/">pages/</a>    
+<br/>
+<a href = "pageeditor.php">pageeditor.php</a>
 </p>
 <style>
     p{
