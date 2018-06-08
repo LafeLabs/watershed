085257fd9514a2a468fc56a44f82dad41b81139d
Lafe Spietz <lafelabs@gmail.com>
2018-06-08 17:40:24 -0400

    updated linkeditor so that it is much easier to use

--- replicators/metamap/linkeditor.php
+++ replicators/metamap/linkeditor.php
@@ -2,20 +2,50 @@
 <html>
     <head>
         <title>Link JSON Editor</title>
+<script>
+function latlon2xy(latlonin) {
+    
+    //input to this function is a string of the form "lattitude,longitude", where lat and lon are in decimal degrees
+    var lat0 = parseFloat(currentJSON.latlon0.split(",")[0]);   //lattitude to 10 meter precision
+    var coslat = Math.cos(lat0*Math.PI/180);//convert to radians, take cosine of lattitude
+    var lon0 =  parseFloat(currentJSON.latlon0.split(",")[1]);//longitude to 10 meter precision
+    var unitfeet = currentJSON.unitfeet;//unit length in feet
+
+    var digits = 1;//1;//0 digits = 555 feet, 1 is 55 feet, 2 is 5.5  feet
+    var precision = Math.pow(10,digits);
+
+    var lat1 = parseFloat(latlonin.split(",")[0]);
+    var lon1 = parseFloat(latlonin.split(",")[1]);
+    //from the original definition of the meter:
+    //90 degrees lattitude = 10,000,000 meters = 32808399 feet
+    //1 degree lattitude = 10,000,000 meters/90 = 111111.1 meters = 111 km
+    //0.1 degree = 11 km
+    //0.01 degree = 1.1 km
+    //0.001 degree = 110 m
+    //0.0001 degree = 11 m
+    //1 meter = 3.28084 feet
+    //1 degree lattitude = 111111.1(meters)*3.28084(feet/meter) = 364538 feet
+    //1 degree longitude = 1 degre lattitude X cos(lattitude) = 283699 feet at 38.8895 N
+    var deltaxfeet = Math.round((lon1 - lon0)*364538*coslat);//convert longitude difference to feet
+    var deltayfeet = Math.round((lat1 - lat0)*364538);//convert lattitude difference to feet
+    var deltax = Math.round(precision*deltaxfeet/unitfeet)/precision;//convert from feet to "units" based on unitfeet
+    var deltay = Math.round(precision*deltayfeet/unitfeet)/precision;//convert from feet to "units" based on unitfeet
+    return deltax.toString() + "," + deltay.toString();
+}
+</script>
     </head>
     <body>
         <div id = "datadiv" style = "display:none">
             <?php
                 echo file_get_contents("json/currentjson.txt");
             ?>
-        </div>    
+        </div>
+    <div id = "page">
+        
         <a id = "editorLink" href = "editor.php">editor.php</a>
         <a id = "indexLink" href = "index.php">index.php</a>
 
         <table id = "mainTable">
-            <tr>
-                <td class = "button">PREV</td><td class = "button">NEXT</td><td  class = "button">NEW</td>
-            </tr>
             <tr>
                 <td>url:</td><td><input/></td>
             </tr>
@@ -37,26 +67,95 @@
         
         <table id = "buttontable">
             <tr>
-                <td class = "button">100'</td>
-                <td class = "button">100'</td>
-                <td class = "button">100'</td>
-                <td class = "button">100'</td>
+                <td class = "button">PREV</td>
+                <td class = "button">NEXT</td>
+                <td  class = "button">NEW</td>
+                <td class = "button">DELETE</td>
+            </tr>
+            <tr>
+                <td class = "button">&#x2bc5</td>
+                <td class = "button">&#x2bc6</td>
+                <td class = "button">&#x2bc7</td>
+                <td class = "button">&#x2bc8</td>
+            </tr>
+            <tr>
+                <td class = "button">&#x2b6f</td>
+                <td class = "button">&#x2b6e</td>
+                <td class = "button">&#x2796</td>
+                <td class = "button">&#x2795</td>
+            </tr>
+            <tr>
+                <td class = "button">3000' &#x2b61</td>
+                <td class = "button">3000' &#x2b63</td>
+                <td class = "button">2500' &#x2b60</td>
+                <td class = "button">2500' &#x2b62</td>
+            </tr>
+            <tr>
+                <td class = "button">300' &#x2b61</td>
+                <td class = "button">300' &#x2b63</td>
+                <td class = "button">250' &#x2b60</td>
+                <td class = "button">250' &#x2b62</td>
+            </tr>
+
+            <tr>
+                <td class = "button">30' &#x2b61</td>
+                <td class = "button">30' &#x2b63</td>
+                <td class = "button">25' &#x2b60</td>
+                <td class = "button">25' &#x2b62</td>
             </tr>
         </table>
+    </div>
         <script>
-            currentJSON = JSON.parse(document.getElementById("datadiv").innerHTML);
-            document.getElementById("textIO").value = JSON.stringify(currentJSON.links,null,"    ");
-            linkIndex = 0;
-            inputs = document.getElementsByTagName("input");
-            buttons = document.getElementsByClassName("button");
+            init();
             redraw();
-            
+
+            function init(){
+                currentJSON = JSON.parse(document.getElementById("datadiv").innerHTML);
+                document.getElementById("textIO").value = JSON.stringify(currentJSON.links,null,"    ");
+                linkIndex = 0;
+                inputs = document.getElementsByTagName("input");
+                buttons = document.getElementsByClassName("button");
+                
+                x0 = innerWidth/2;
+                y0 = innerHeight/2;    
+                unit = currentJSON.unitpixels;
+                document.getElementById("mainImage").src = currentJSON.imgurl;
+                for(var index = 0;index < currentJSON.links.length;index++){
+                    var newa = document.createElement("a");
+                    newa.className = "links";
+                    newa.innerHTML = currentJSON.links[index].text;
+                    newa.href = currentJSON.links[index].url;
+                    document.getElementById("page").appendChild(newa);
+                }
+                links = document.getElementsByClassName("links");
+                
+            }
             function redraw(){
                 inputs[0].value = currentJSON.links[linkIndex].url;
                 inputs[1].value = currentJSON.links[linkIndex].text;
                 inputs[2].value = currentJSON.links[linkIndex].latlon;
                 inputs[3].value = currentJSON.links[linkIndex].fontfeet;
                 inputs[4].value = currentJSON.links[linkIndex].rotation;
+                
+                document.getElementById("mainImage").style.width = (currentJSON.imgw*unit).toString()  + "px";
+                document.getElementById("mainImage").style.left = (x0 + currentJSON.imgleft*unit).toString()  + "px";
+                document.getElementById("mainImage").style.top = (y0 + currentJSON.imgtop*unit).toString()  + "px";
+                document.getElementById("mainImage").style.transform = "rotate(" + currentJSON.imgangle.toString() +"deg)";
+
+
+                for(var index = 0;index < links.length;index++){
+                    var xy = latlon2xy(currentJSON.links[index].latlon);
+                    var xvar = parseFloat(xy.split(",")[0]);
+                    links[index].style.left = (x0 + unit*xvar).toString() + "px";
+                    var yvar = parseFloat(xy.split(",")[1]);
+                    links[index].style.top = (y0 - unit*yvar).toString() + "px";
+                    links[index].style.fontSize = (currentJSON.links[index].fontfeet*unit/currentJSON.unitfeet).toString() + "px";
+                    links[index].style.transform = "rotate(" + currentJSON.links[index].rotation + "deg)";
+                    links[index].style.border = "none";
+
+                }
+
+                links[linkIndex].style.border = "solid";
             }
             function redraw2(){
                     document.getElementById("textIO").value = JSON.stringify(currentJSON.links,null,"    ");
@@ -66,6 +165,7 @@
                     httpc.open("POST", url, true);
                     httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
                     httpc.send("data="+data+"&filename=json/currentjson.txt");//send text to filesaver.php
+                    redraw();
             }
             
             inputs[0].onchange = function(){
@@ -81,11 +181,11 @@
                     redraw2();
             }
             inputs[3].onchange = function(){
-                    currentJSON.links[linkIndex].fontfeet = this.value;
+                    currentJSON.links[linkIndex].fontfeet = parseFloat(this.value);
                     redraw2();
             }
             inputs[4].onchange = function(){
-                    currentJSON.links[linkIndex].rotation = this.value;
+                    currentJSON.links[linkIndex].rotation = parseFloat(this.value);
                     redraw2();
             }
 
@@ -104,7 +204,7 @@
                 }               
                 redraw();
             }
-    
+
             buttons[2].onclick  = function(){
                 var foo = JSON.stringify(currentJSON.links[0],null,"    ");
                 var zeep = JSON.parse(foo);
@@ -118,7 +218,158 @@
                 httpc.send("data="+data+"&filename=json/currentjson.txt");//send text to filesaver.php
                 redraw();
             }
+            buttons[3].onclick = function(){
+                //delete current link    
+            }
+            buttons[4].onclick = function(){
+                y0 -= 50;
+                redraw2();
+            }
+            buttons[5].onclick = function(){
+                y0 += 50;
+                redraw2();
+            }
+            buttons[6].onclick = function(){
+                x0 -= 50;
+                redraw2();
+            }
+            buttons[7].onclick = function(){
+                x0 += 50;
+                redraw2();
+            }
+            
+            buttons[8].onclick = function(){
+                currentJSON.links[linkIndex].rotation -= 5;
+                redraw2();
+            }
+            buttons[9].onclick = function(){
+                currentJSON.links[linkIndex].rotation += 5;
+                redraw2();
+            }
 
+            buttons[10].onclick = function(){
+                unit /= 1.3;
+                redraw2();
+            }
+            buttons[11].onclick = function(){
+                unit *= 1.3;
+                redraw2();
+            }
+
+            buttons[12].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallat += 0.01;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+            buttons[13].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallat -= 0.01;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+
+            buttons[14].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallon -= 0.01;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+            buttons[15].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallon += 0.01;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+            
+            buttons[16].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallat += 0.001;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+            buttons[17].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallat -= 0.001;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+
+            buttons[18].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallon -= 0.001;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+            buttons[19].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallon += 0.001;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+            
+            buttons[20].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallat += 0.0001;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+            buttons[21].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallat -= 0.0001;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+
+            buttons[22].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallon -= 0.0001;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+            buttons[23].onclick = function(){
+                var locallat = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[0]);
+                var locallon = parseFloat(currentJSON.links[linkIndex].latlon.split(",")[1]);
+                locallon += 0.0001;
+                locallat = Math.round(locallat*100000)*0.00001;
+                localon = Math.round(locallon*100000)*0.00001;
+                currentJSON.links[linkIndex].latlon = locallat.toFixed(5) + "," + locallon.toFixed(5);
+                redraw2();
+            }
+            
 document.getElementById("textIO").onkeyup = function(){
     currentJSON.links = JSON.parse(document.getElementById("textIO").value);
     data = encodeURIComponent(JSON.stringify(currentJSON,null,"    "));
@@ -175,7 +426,34 @@ document.getElementById("textIO").onkeyup = function(){
                 position:absolute;
                 bottom:5px;
                 left:5px;
+                border-collapse:collapse;
             }
+            #buttontable td{
+                border:solid;
+                text-align:center;
+            }
+            #mainImage{
+                position:absolute;
+                z-index:-2;
+            }
+#page{
+    width:100%;
+    height:100%;
+    position:absolute;
+    left:0px;
+    top:0px;
+    overflow:hidden;
+}
+
+.links{
+    position:absolute;
+    color:blue;
+    background-color:white;
+    z-index:1;
+    font-family:"Arial Black", Gadget, sans-serif;
+
+}
+
         </style>
     </body>
 </html>
